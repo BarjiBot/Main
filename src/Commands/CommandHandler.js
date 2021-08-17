c16361cfe7da944b1a6dd2773240e4e2588bc9b0
MikeyPants <michael.a.hazan@gmail.com>
2021-08-18 02:45:33 +0300

    SETUP, CommandHandler start, Token.json is now a config file, further work on command handler will be done soon

---
+++ src/Commands/CommandHandler.js
@@ -0,0 +1,10 @@
+const { Message } = require("discord.js");
+
+module.exports = async function(msg = new Message()){
+	console.log(msg);
+};
+
+
+
+
+//`Yes, Test went Poggers ${Date.now() - message.createdTimestamp}`
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,28 @@
+
+const fs = require('fs');
+
 // Token import
-const Token = require('./Credentials/Token');
-TOKEN = Token.GetToken();
+const { Token } = require('./Credentials/Config');
+TOKEN = Token;
 
 // Discord JS import
-const { Client, Intents } = require('discord.js');
-const bot = new Client({ intents: [Intents.FLAGS.DIRECT_MESSAGES] });
+const { Client, Collection, Intents, Interaction } = require('discord.js');
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const CommandHandler = require('./Commands/CommandHandler');
+
+bot.once('ready', () => {
+	console.log(`${bot.user.tag} has been activated succesfully`);
+}); // START UP MESSAGE AND SETUP
+
+bot.on('messageCreate', msg => {CommandHandler(msg)});
+
+
+
 
 bot.login(TOKEN); // Bot Start Up
 
-//jkjj
+
 
 
 
