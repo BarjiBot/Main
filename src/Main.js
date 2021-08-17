65fef2c2ecb88dae1533f8eb6966df05fe478297
MikeyPants <michael.a.hazan@gmail.com>
2021-08-17 16:29:57 +0300

    Added Run.bat (Runs the bot for tests) and setted up basic stuff

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,12 @@
+// Token import
 const Token = require('./Token');
+TOKEN = Token.GetToken();
+
+// Discord JS import
+const { Client } = require('discord.js');
+const bot = new Client({ intents: ['DIRECT_MESSAGES'] });
+
+bot.login(TOKEN);
+
 
 
