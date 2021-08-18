fc035373282df512ccce0dfd3e9edef6d9b575cf
MikeyPants <michael.a.hazan@gmail.com>
2021-08-18 20:22:25 +0300

    MessageHandler Update

--- src/Messages/MessageHandler.js
+++ src/Messages/MessageHandler.js
@@ -4,8 +4,3 @@ const { Message } = require("discord.js");
 module.exports = (async function(msg = new Message()){
 	console.log(msg.content);
 });	
-
-
-
-
-//`Yes, Test went Poggers ${Date.now() - message.createdTimestamp}`
