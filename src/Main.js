3886f0512fb9346ac98a0c7f6ae8d4868731d544
MikeyPants <michael.a.hazan@gmail.com>
2021-08-18 20:17:46 +0300

    polishing code #1

--- src/Main.js
+++ src/Main.js
@@ -4,12 +4,18 @@ const { Token } = require('./Credentials/Config');
 TOKEN = Token;
 
 // Discord JS import
-const { Client, Collection, Intents, Interaction } = require('discord.js');
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents} = require('discord.js');
+const intentsList = [
+	Intents.FLAGS.GUILDS, 
+	Intents.FLAGS.GUILD_MEMBERS, 
+	Intents.FLAGS.GUILD_BANS, 
+	Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS
+	/* If you see this it means you didn't close off this list, you can do that by clicking the arrow beside the const */
+];
+const bot = new Client({ intents: intentsList });
 
 
 // Command Handler import - Handles command files and executes them at send of message
-const CommandFiles = require('./Commands/Handlers/CommandFiles');
 const CommandHandler = require('./Commands/Handlers/CommandHandler');
 
 
