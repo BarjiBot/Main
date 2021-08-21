a78e9baeed9ef94ed071754957c79657e4192bee
MikeyPants <michael.a.hazan@gmail.com>
2021-08-21 18:43:47 +0300

    Messages Changes

--- src/Commands/AddPermittedRole.js
+++ src/Commands/AddPermittedRole.js
@@ -1,4 +1,4 @@
-const { Message } = require('discord.js');
+const { Message, Permissions } = require('discord.js');
 const { interact } = require('../Messages/ButtonsInteraction');
 const { ButtonConfirmDeny, EmbedNoPerm, EmbedMissingArgs, EmbedNeedConfirmation, EmbedSuccess, EmbedNoNeed} = require('../Messages/Messages');
 const { SAVES, YESNO } = require('../Credentials/Config.json');
@@ -11,7 +11,7 @@ var n = "addpermrole";
 function messageSent(msg = new Message()){
 	if(msg.mentions.roles.first() && !Object.values(SaveFile[msg.guildId][SAVES.Roles]).find(val => val === msg.mentions.roles.first().id.toString())){
 		EmbedNeedConfirmation
-			.setDescription(`This role will be added to the mod commands permitted roles
+			.setDescription(`${msg.mentions.roles.first()} will be added to the mod commands permitted roles
 			\n(you can remove it later on)`)
 			.setAuthor(msg.author.username, msg.author.avatarURL());
 
@@ -31,7 +31,7 @@ function messageSent(msg = new Message()){
 	else if(msg.mentions.roles.first()){
 		EmbedNoNeed.setDescription(`This Role Has Already Been Added! 
 		\n(If you wish to remove it use -removepermrole)`)
-
+		.setAuthor(msg.author.username, msg.author.avatarURL());
 		msg.reply({ embeds: [EmbedNoNeed]}).then(mesg => {msg.delete();});
 		return;
 	} else {
@@ -49,7 +49,7 @@ function messageSent(msg = new Message()){
 module.exports = ({
 	name : n,
 	async execute(msg = new Message()){
-		if(msg.member.roles.cache.find(r => Object.values(SaveFile[msg.guildId][SAVES.Roles]).includes(r.id)))
+		if(msg.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) || msg.member.roles.cache.find(r => Object.values(SaveFile[msg.guildId][SAVES.Roles]).includes(r.id)))
 			await messageSent(msg);
 		else
 			msg.member.send({ embeds: [EmbedNoPerm]}).then(bmsg => {msg.delete();});
