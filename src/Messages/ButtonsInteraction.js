479d6bac28b7acaa0052361cdf66d68f005fec82
MikeyPants <michael.a.hazan@gmail.com>
2021-12-30 19:38:35 +0200

    Changes

--- src/Messages/ButtonsInteraction.js
+++ src/Messages/ButtonsInteraction.js
@@ -1,7 +1,7 @@
 const { Message } = require("discord.js");
 const { EmbedNotUser, EmbedSuccess, EmbedCanceled } = require("./Messages");
 const { SAVES, YESNO, ROLES } = require('../Credentials/Config.json');
-const Saving = require('../Save/Save_File');
+
 
 
 function interact(msg = new Message(), bmsg = new Message()){
@@ -25,29 +25,7 @@ function interact(msg = new Message(), bmsg = new Message()){
 		}
 		id = ButtonInteraction.first().customId.split(',');
 		
-		if(id[1] == SAVES.Permissions){
-			EmbedSuccess.setAuthor(msg.author.username, msg.author.avatarURL());
-			bmsg.edit({ embeds: [EmbedSuccess], components: [] });
-			Saving.EXECUTESAVE(msg.guild.id, SAVES.Permissions, id[2]);
-		}
 		
-		if(id[1] == SAVES.Channels){
-			if(id[3] == YESNO.CONFIRM){
-				EmbedSuccess.setAuthor(msg.author.username, msg.author.avatarURL());
-				bmsg.edit({ embeds: [EmbedSuccess], components: [] });
-				Saving.EXECUTESAVE(msg.guild.id, SAVES.Channels, id[2]).then(r =>{
-					var linkRole = msg.guild.roles.cache.find(r => r.name === ROLES.Link);
-					var channel = msg.guild.channels.cache.find(c => c.id === id[2].split(':')[1]);
-					console.log(channel.name);
-					channel.permissionOverwrites.create(msg.guild.roles.everyone,{'SEND_MESSAGES': false});
-					channel.permissionOverwrites.create(linkRole,{'SEND_MESSAGES': true});
-			
-				});
-			}else{
-				Saving.REMOVE(msg.guild.id, SAVES.Channels, id[2]);
-				bmsg.edit({ embeds: [EmbedSuccess], components: [] });
-			}
-		}
 	});
 
 }
