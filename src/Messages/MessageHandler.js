cb87010ab4bb123cf0b6459c3684cc729189cbfc
MikeyPants <michael.a.hazan@gmail.com>
2021-12-30 19:09:44 +0200

    Remaking the bot, lets get it

--- src/Messages/MessageHandler.js
+++ src/Messages/MessageHandler.js
@@ -1,19 +1,10 @@
 const { Message } = require("discord.js");
 const { ID, SAVES, ROLES } = require('../Credentials/Config.json');
-const Saving = require('../Save/Save_File.js');
-const SaveFile = require('../Save/Save_File.json');
 const { EmbedInvitesNotAllowed, EmbedLinksNotAllowed } = require("./Messages");
 
 var iperms = 250, lperms = 250;
 
-function RunSaves(user, guildID){
 
-	if(!user.roles.cache.find(r => r.id === SaveFile[guildID][SAVES.Roles][ROLES.Image]) && SaveFile[guildID][SAVES.Messages][user.id] >= iperms)
-		user.roles.add(SaveFile[guildID][SAVES.Roles][ROLES.Image]);
-
-	if(!user.roles.cache.find(r => r.id === SaveFile[guildID][SAVES.Roles][ROLES.Link]) && SaveFile[guildID][SAVES.Messages][user.id] >= lperms)
-		user.roles.add(SaveFile[guildID][SAVES.Roles][ROLES.Link]);
-}
 function Run(msg = new Message()){
 	if(!msg.member) return;
 	if(msg.member.id != ID){
@@ -22,27 +13,17 @@ function Run(msg = new Message()){
 			msg.author.send({embeds: [EmbedInvitesNotAllowed]}).then(mesg => {msg.delete()});
 			return;
 		}
-		if(msg.content.includes('https://'||'http://'||'www.') && !JSON.stringify(SaveFile[msg.guildId][SAVES.Channels]).includes(msg.channelId.toString())){
+		if(msg.content.includes('https://'||'http://'||'www.')){
 			EmbedLinksNotAllowed.setAuthor(msg.guild.name, msg.guild.iconURL());
-			EmbedLinksNotAllowed.setDescription(`Unfortunately you cant send messages at ${msg.channel},
-			\n you can only send links in: <#${Array.from(Object.values(SaveFile[msg.guildId][SAVES.Channels])).join('>, <#')}>`)
+			EmbedLinksNotAllowed.setDescription(`Unfortunately you cant send messages at ${msg.channel}`)
 			msg.author.send({embeds: [EmbedLinksNotAllowed]}).then(mesg => {msg.delete()});
 			return;
-		}
-
-		// The first message the bot recives will setup the SaveFile of the server
-		if(!SaveFile[msg.guild.id.toString()]) Saving.SETUP(msg.guild);
-		else
-		{
-			if(!SaveFile[msg.guildId][SAVES.Roles][ROLES.Link])Saving.ROLESSETUP(msg.guild);
-			Saving.EXECUTEMESSAGESAVE(msg.guildId, msg.author.id);
-			RunSaves(msg.member, msg.guild.id);
 		}
 		
 	}
 }
 
-module.exports = {Run, RunSaves};	
+module.exports = {Run};	
 
 
 //&& msg.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR || Permissions.FLAGS.EMBED_LINKS)
